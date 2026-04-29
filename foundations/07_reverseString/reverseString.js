47547a827acd1e14b87455e7651b0b204422fae2
Joshua Toledo <joshuapogi123123123@gmail.com>
2026-04-29 21:40:47 +0800

    Passed all the test in reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let words = string.split('').reverse().join('')
+  return words;
 };
 
 // Do not edit below this line
