9a82e122bf8cf2aef17fe4186eacabf0ee82fb13
Joshua Toledo <joshuapogi123123123@gmail.com>
2026-04-29 21:25:28 +0800

    Passed all the test in repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,5 @@
 const repeatString = function(string,num) {
-  if(num === 0) return "ERROR";
+  if(num < 0) return "ERROR";
   let result = "";
   for (let i = 0; i < num ; i++ ) {
     result += string;
