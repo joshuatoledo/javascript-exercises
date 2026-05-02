2ae2018eada8abf04a9a637adf3a17111ae02b6a
Joshua Toledo <joshuapogi123123123@gmail.com>
2026-05-02 18:32:35 +0800

    passed all the test in sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
 const sumAll = function(start,end) {
+    
+    if(start < 0 || end < 0){
+        return 'ERROR';
+    }
+
+     if (!Number.isInteger(start) || !Number.isInteger(end)){
+        return 'ERROR';    
+     }
+     
+  const min = Math.min(start, end);
+  const max = Math.max(start, end);
+  
   let arr = [];
-  for(let i = start; i <= end; i++){
+  for(let i = min; i <= max; i++){
       arr.push(i);
   }
   return arr.reduce((acc, curr) => acc + curr ,0);
