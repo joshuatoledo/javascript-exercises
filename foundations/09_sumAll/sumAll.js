8a3a7a15f7854e05a91377d5e0c415b9a36683c6
Joshua Toledo <joshuapogi123123123@gmail.com>
2026-05-02 18:07:26 +0800

    write some code in the sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+  let arr = [];
+  for(let i = start; i <= end; i++){
+      arr.push(i);
+  }
+  return arr.reduce((acc, curr) => acc + curr ,0);
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
