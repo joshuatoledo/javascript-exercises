d60213efbb1acf22a894fb82fa9c51728c6e62ca
Joshua Toledo <joshuapogi123123123@gmail.com>
2026-05-05 21:37:34 +0800

    passed all tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  
+    let rounded = (fahrenheit - 32 ) * (5/9);
+      return Math.round(rounded * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  let rounded = (celsius * (9 / 5) + 32);
+  
+  return Math.round(rounded * 10) / 10;
 };
 
 // Do not edit below this line
