776bd277a2db604045a43dda9727cd141f8d1562
Joshua Toledo <joshuapogi123123123@gmail.com>
2026-05-31 21:32:10 +0800

    practice

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
+// Do not edit below this line
 module.exports = fibonacci;
+
+//   const sequence = [0, 1];
+//   for (let i = 2; i < n; i++) {
+//     sequence[i] = sequence[i - 1] + sequence[i - 2];
+//   }
+//   return sequence.slice(0, n);
