b779aa893464108b3bc00d2f8ccdc79141c0899a
Joshua Toledo <joshuapogi123123123@gmail.com>
2026-05-31 20:16:23 +0800

    implement palindrome function

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (palindromes) {
+  const word = palindromes.toLowerCase().replace(/[^a-z0-9]/gi, "");
+  const reverseWord = word.split("").reverse().join("");
 
+  if (word === reverseWord) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
