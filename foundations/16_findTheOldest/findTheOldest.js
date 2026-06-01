54eee131bbde96d78b29da147e3417db826be7d7
Joshua Toledo <joshuapogi123123123@gmail.com>
2026-06-01 16:54:38 +0800

    feat: implement getOldest function with support for living people

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (age) {
+  return age.sort((a, b) => {
+    const currentYear = new Date().getFullYear();
+    const lastGuy = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+    const newGuy = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+    return lastGuy > newGuy ? -1 : 1;
+  })[0];
 };
 
 // Do not edit below this line
